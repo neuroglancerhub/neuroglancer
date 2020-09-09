6b88371d5f8c424c5a7171083e7bb38b06b46727
Jody Clements <jody@neomorphic.co.uk>
2020-09-09 10:28:02 -0400

    fix(ui/annotation): backwards compatibility with source string
    The checks added to see if the source url starts with a clio protocol
    string were not backwards compatible with the source being a string and
    not an object containing a url key.

--- src/neuroglancer/ui/annotations.ts
+++ src/neuroglancer/ui/annotations.ts
@@ -2102,8 +2102,13 @@ export function UserLayerWithAnnotationsMixin<TBase extends {new (...args: any[]
           specification[SHADER_CONTROLS_JSON_KEY]);
       this.annotationDefaultProperty.restoreState(specification[ANNOTATION_DEFAULT_PROPERTY_JSON_KEY]);
 
-      if ('source' in specification ) { //tmp hack
-        let url = ('url' in specification['source']) ? specification.source.url : specification.source;
+      if ('source' in specification ) { // tmp hack
+        let url;
+        if (typeof(specification.source) === 'string') {
+          url = specification.source;
+        } else if ('url' in specification['source']) {
+          url = specification.source.url;
+        }
         if (typeof(url) === 'string' && url.startsWith('clio://')) {
           this.userFilterNeeded = true;
         }
